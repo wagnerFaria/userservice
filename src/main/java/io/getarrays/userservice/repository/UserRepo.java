4b5f1a532ad0adf7a048bbc6a0860ca620ff7dc7
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2022-11-24 16:44:00 -0400

    Changing default config of spring security for UserDetails

--- src/main/java/io/getarrays/userservice/repository/UserRepo.java
+++ src/main/java/io/getarrays/userservice/repository/UserRepo.java
@@ -3,7 +3,9 @@ package io.getarrays.userservice.repository;
 import io.getarrays.userservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -6,19 +6,37 @@ import io.getarrays.userservice.repository.RoleRepo;
 import io.getarrays.userservice.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.getUser(username);
+        log.info("User found in database: {}", user);
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        user
+                .getRoles()
+                .forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -35,7 +53,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void addRoleToUser(String username, String roleName) {
         log.info("Adding role {} to user {}", roleName, username);
-        User user = userRepo.findByUsername(username);
+        User user = this.getUser(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
     }
@@ -43,7 +61,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String username) {
         log.info("Fetching user {}", username);
-        return userRepo.findByUsername(username);
+        return userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found in database"));
     }
 
     @Override
