4b3d5c331c182e13dc34b93455735b188e85f197
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2022-11-24 15:43:00 -0400

    Adding USERS and ROLES

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -1,12 +1,7 @@
 package io.getarrays.userservice;
 
-import io.getarrays.userservice.domain.Role;
-import io.getarrays.userservice.domain.User;
-import io.getarrays.userservice.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserserviceApplication {
