dc151c22c65c8d215ac3748e38f66c7a7b8bab0c
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2022-11-24 14:41:40 -0400

    Creating User Resource and docker-compose

---
+++ src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -0,0 +1,23 @@
+package io.getarrays.userservice.api;
+
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
