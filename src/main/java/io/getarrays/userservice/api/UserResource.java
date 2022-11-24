096a60696f7538c86edafc3a4aa0b7cfe400f528
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2022-11-24 15:23:39 -0400

    Adding getUsers, saveUser, saveRole and AddRoleToUser to User Resource

--- src/main/java/io/getarrays/userservice/api/UserResource.java
+++ src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -1,13 +1,15 @@
 package io.getarrays.userservice.api;
 
+import io.getarrays.userservice.domain.Role;
 import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,4 +22,26 @@ public class UserResource {
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        return new ResponseEntity<Role>(userService.saveRole(role), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> AddRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.username, form.roleName);
+        return ResponseEntity.ok().build();
+    }
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
