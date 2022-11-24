d9da1b31756ddc7b154da070b362d7d06d14d762
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2022-11-24 12:25:43 -0400

    creating User and Role Services

---
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package io.getarrays.userservice.service;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.repository.RoleRepo;
+import io.getarrays.userservice.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
